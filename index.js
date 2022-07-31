37e5a3352dadcef9f56dd518bfc95c7b6b7a067a
THANAI <tanai.saeng@gmail.com>
2022-07-31 16:53:08 +0700

    connect mongoDb

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Disconnected from MongoDB");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected");
+});
 
 app.listen(8000, () => {
+  connect();
   console.log("Server is running on port 8000");
 });
