9fb503ec1537af97f15c0ea1594e8ca2861ebece
THANAI <tanai.saeng@gmail.com>
2022-08-04 21:34:51 +0700

    fix model activity

--- models/activity.js
+++ models/activity.js
@@ -22,6 +22,10 @@ const ActivitySchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 export default mongoose.model("Activity", ActivitySchema);
